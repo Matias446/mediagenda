f6a92e133b93df62094aa4f1971b5a0cfce3f938
Matias <pineyromatias44@gmail.com>
2026-08-22 20:20:06 -0300

    Fix CORS policy to allow Vercel production domain

--- mediAgenda.WebAPI/Program.cs
+++ mediAgenda.WebAPI/Program.cs
@@ -34,7 +34,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "https://mediagenda-sand.vercel.app"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
